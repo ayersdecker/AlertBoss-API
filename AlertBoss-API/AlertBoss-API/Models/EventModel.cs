999eb5d4970b04a740afd54dddb045ec6b67f567
Decker Ayers <ayersdecker@gmail.com>
2023-06-06 11:38:34 -0400

    EventModel Updated

--- AlertBoss-API/AlertBoss-API/Models/EventModel.cs
+++ AlertBoss-API/AlertBoss-API/Models/EventModel.cs
@@ -2,9 +2,21 @@
 
 namespace AlertBoss_API.Models;
 
-[Table("event")]
+[Table("Events")]
 public class EventModel
 {
-    [Column("id")]
+    [Column("eventId")]
     public int Id { get; set; }
+    [Column("eventType")]
+    public string EventType { get; set; }
+    [Column("color")]
+    public string Color { get; set; }
+    [Column("blink")]
+    public int Blink { get; set; }
+    [Column("output_email")]
+    public bool OutputEmail { get; set; }
+    [Column("output_text")]
+    public bool OutputText { get; set; }
+    [Column("deviceId")]
+    public string DeviceId { get; set; }
 }
