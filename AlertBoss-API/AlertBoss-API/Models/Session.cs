386529203294722ee03daf419baeefa0062974d6
Decker Ayers <ayersdecker@gmail.com>
2023-06-06 11:41:46 -0400

    Session Updated

---
+++ AlertBoss-API/AlertBoss-API/Models/Session.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AlertBoss_API.Models
+{
+    [Table("Session")]
+    public class Session
+    {
+        [Column("uid")]
+        public int UID { get; set; }
+        [Column("session_key")]
+        public int Key { get; set; }
+    }
+}
