f385c3b305854cae412fa219b80d57114b978833
Decker Ayers <ayersdecker@gmail.com>
2023-06-06 12:41:28 -0400

    Alert Update

--- AlertBoss-API/AlertBoss-API/Models/Alert.cs
+++ AlertBoss-API/AlertBoss-API/Models/Alert.cs
@@ -2,9 +2,17 @@
 
 namespace AlertBoss_API.Models;
 
-[Table("alert")]
+[Table("Alerts")]
 public class Alert
 {
-    [Column("id")]
+    [Column("alertId")]
     public int Id { get; set; }
+    [Column("eventType")]
+    public string EventType{ get; set; }
+    [Column("alrtName")]
+    public string Name { get; set; }
+    [Column("location")]
+    public string Location { get; set; }
+    [Column("severity")]
+    public string Severity { get; set; }
 }
